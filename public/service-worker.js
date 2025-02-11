15f6d0b2b6a562b80f362a3c506efe914b26cfc9
Ronish-Prajapati <doggylusi@gmail.com>
2025-02-11 13:06:18 +0545

    workerservice adder

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+// public/service-worker.js
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/App.jsx',
+          '/styles.css',
+          
+          // Add other assets here
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
